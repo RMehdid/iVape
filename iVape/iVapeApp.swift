63d5e5b401059d445816e59117a3ee832bbe77be
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-08-26 20:52:33 +0100

    Initial Commit

---
+++ iVape/iVapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iVapeApp.swift
+//  iVape
+//
+//  Created by Samy Mehdid on 26/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct iVapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
